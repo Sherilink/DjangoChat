1c090311cc7193421c38af80f5903fc2e65c2ccc
Sherilink <146932275+Sherilink@users.noreply.github.com>
2025-10-26 17:19:18 +0530

    Update views.py

--- chat/chatbot/views.py
+++ chat/chatbot/views.py
@@ -5,7 +5,7 @@ import logging
 from dotenv import load_dotenv
 import threading
 from . import download_model
-import chatbot.download_model
+import threading
 
 threading.Thread(target=download_model.download_model).start()
 
@@ -339,6 +339,7 @@ def delete_all_chats(request):
         Message.objects.filter(thread__user=request.user).delete()
         return JsonResponse({"status": "success", "message": "All chats deleted."})
     return JsonResponse({"error": "Invalid request"}, status=400)
+
 
 
 
